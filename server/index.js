c17fb4afb643c2ae53dee40423357c5450ae5f93
Hayk Yolyan <54978371+Narberal-Gamma@users.noreply.github.com>
2023-06-03 10:50:55 +0400

    changed server settings

--- server/index.js
+++ server/index.js
@@ -1,1 +1,19 @@
-ÿþ
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors') 
+
+const PORT = process.env.PORT || 5000
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const start = () => {
+    try {
+        app.listen(PORT, () => console.log(`Server started on PORT ${PORT}`))
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+start()
